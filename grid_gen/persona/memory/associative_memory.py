989c0491d0080e487bb7fdaab69fb2b6b83c17ef
jinlee-sori <jinlee@berkeley.edu>
2025-11-23 00:02:07 -0800

    Update associative_memory.py
    social aspects updated

--- grid_gen/persona/memory/associative_memory.py
+++ grid_gen/persona/memory/associative_memory.py
@@ -1,4 +1,6 @@
-
+#---------------------------------------------------------------
+#import re # to extract coordinates (x, y) from shared hazard strings
+#---------------------------------------------------------------
 
 def seed_static_memory(agent):
     cfg = agent.config
@@ -17,3 +19,29 @@ def seed_static_memory(agent):
             importance=10,   # max/near-max
             tags=["identity", "social", "dependents"],
         )
+
+#---------------------------------------------------------------
+# added for social
+#import re  # extract coordinates (x, y) from shared hazard 
+#
+#def add_social_memory(agent, info: str):
+#
+#    # store the message 
+#    agent.assoc_mem.add_fact(
+#        text=f"Friend said: {info}",
+#        importance=6,  # moderate importance
+#        tags=["social", "shared_info", "hazard"],  
+#    )
+#
+#    # extract coordinates 
+#    m = re.search(r"\((\d+),\s*(\d+)\)", info)
+#    if m:
+#        x = int(m.group(1))
+#        y = int(m.group(2))
+#
+#        if not hasattr(agent, "known_hazard_cells"):
+#            agent.known_hazard_cells = set()
+#
+#        # add  hazard location to the agent’s hazard memory
+#        agent.known_hazard_cells.add((x, y))
+#---------------------------------------------------------------
